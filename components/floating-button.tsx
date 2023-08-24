01494cde8ea88060c48d741d316d682ed9318bdf
b0jun <bjkim0228@naver.com>
2023-08-24 13:49:00 +0900

    repo 수정

--- components/floating-button.tsx
+++ components/floating-button.tsx
@@ -1,17 +1,18 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
 
 interface FloatingButton {
-  children: React.ReactNode;
-  href: string;
+	children: React.ReactNode;
+	href: string;
 }
 
 export default function FloatingButton({ children, href }: FloatingButton) {
-  return (
-    <Link href={href}>
-      <a className="fixed hover:bg-orange-500 border-0 aspect-square border-transparent transition-colors cursor-pointer  bottom-24 right-5 shadow-xl bg-orange-400 rounded-full w-14 flex items-center justify-center text-white">
-        {children}
-      </a>
-    </Link>
-  );
+	return (
+		<Link
+			href={href}
+			className="fixed flex items-center justify-center text-white transition-colors bg-orange-400 border-0 border-transparent rounded-full shadow-xl cursor-pointer hover:bg-orange-500 aspect-square bottom-24 right-5 w-14"
+		>
+			{children}
+		</Link>
+	);
 }
